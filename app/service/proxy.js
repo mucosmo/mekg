783d5750a11b40a7f4943faaaa14d4f6c0a650a0
Cosmo <mfuture@qq.com>
2021-10-21 17:44:41 +0800

    new file

---
+++ app/service/proxy.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: mfuture@qq.com
+ * @Date: 2021-10-21 16:19:31
+ * @Description: 代理ip
+ */
+
+const query = require('../../config/mysql/local')
+
+
+module.exports = selectProxy = async () => {
+
+    sql = `select * from ip_proxy where available=1`
+
+    results = await query(sql)
+
+    return results
+
+}
